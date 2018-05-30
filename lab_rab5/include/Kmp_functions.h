15672032f3a2ff6e790bdd18867fb7f24c750fff
valeriaivankova98 <valeriaivankova94@gmail.com>
2018-05-31 00:16:25 +0300

    Update Kmp_functions.h

--- lab_rab5/include/Kmp_functions.h
+++ lab_rab5/include/Kmp_functions.h
@@ -6,8 +6,8 @@
 
 void print(const std::vector<int>& index);
 
-std::vector<int> prefix(std::string str);
+std::vector<int> prefix(std::string& str);
 
-std::vector<int> kmp(std::string str, std::string obr, const std::vector<int>& pi);
+std::vector<int> kmp(std::string& str, std::string& obr, const std::vector<int>& pi);
 
-int shift(std::string str, std::string obr, const std::vector<int>& pi);
+int shift(std::string& str, std::string& obr, const std::vector<int>& pi);
